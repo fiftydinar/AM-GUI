633a90ebc94d707a53c45e685bb9722bc6b941eb
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-10-01 21:06:33 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { exec } = require('child_process');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 750,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.handle('am-action', async (event, action, software) => {
+  let command = '';
+  if (action === 'install') {
+    command = `am -i "${software}"`;
+  } else if (action === 'uninstall') {
+    command = `am -r "${software}"`;
+  }
+  return new Promise((resolve) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) resolve(stderr || err.message);
+      else resolve(stdout);
+    });
+  });
+});
+
+ipcMain.handle('list-apps', async () => {
+  return new Promise((resolve) => {
+    exec('am -l', (err, stdout, stderr) => {
+      if (err) resolve([]);
+      else {
+        const apps = stdout.split('\n')
+          .map(line => line.trim())
+          .filter(line => line.length > 0);
+        resolve(apps);
+      }
+    });
+  });
+});
