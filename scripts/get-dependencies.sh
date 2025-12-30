3fee8d2221471f01e4256e06ca3478d9fd21ce91
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-12-30 23:28:10 +0100

    ci: fix AppImage workflow (artifact upload/download) and update get-dependencies script)

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -19,12 +19,12 @@ get-debloated-pkgs --add-common --prefer-nano
 # If the application needs to be manually built that has to be done down here
 
 # Télécharger et installer nodejs/npm via nvm :
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 22
-node -v # Doit afficher "v22.21.1".
-npm -v # Doit afficher "10.9.4".
-npm install
-npm run dist
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+#\. "$HOME/.nvm/nvm.sh"
+#nvm install 22
+#node -v # Doit afficher "v22.21.1".
+#npm -v # Doit afficher "10.9.4".
+#npm install
+#npm run dist
 
 
