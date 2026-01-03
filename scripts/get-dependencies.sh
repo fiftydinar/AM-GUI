dd4d46b6088a0ca26c4e7b621ca191eeae14f103
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-03 12:36:18 +0100

    fix: harmonisation structure AppDir, desktop et workflow
    - Ajout de curl à l'étape d'installation des outils
    - Correction du chemin Exec dans AM-GUI.desktop (am-gui %U)
    - Correction du chemin de déploiement dans get-dependencies.sh (linux-unpacked -> AppDir)
    - quick-sharun appelé sur AppDir/bin dans make-appimage.sh
    - Nettoyage des commentaires inutiles

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -11,22 +11,14 @@ pacman -Syu --noconfirm python
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+#get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
 
-# Télécharger et installer nodejs/npm via nvm :
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-#\. "$HOME/.nvm/nvm.sh"
-#nvm install 22
-#node -v # Doit afficher "v22.21.1".
-#npm -v # Doit afficher "10.9.4".
-#npm install
-#npm run dist
 mkdir -p ./AppDir/bin
-cp -rv dist/linux-unpacked/*    ./AppDir/bin
+cp -rv dist/linux-unpacked/*    ./AppDir
 cp -v  AM-GUI.png               ./AppDir/.DirIcon
 cp -v  AM-GUI.desktop           ./AppDir/
