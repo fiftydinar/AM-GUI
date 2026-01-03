03b1000cdde2bd53d9dac90dfaa6f7b3ebb73d12
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-03 21:50:47 +0100

    fix: test quick-sharun sur .DirIcon pour debug AppRun/AppImage

--- scripts/make-appimage.sh
+++ scripts/make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
 # Deploy dependencies
-quick-sharun  ./AppDir/bin/*
+quick-sharun  ./AppDir/.DirIcon
 
 # Additional changes can be done in between here
 
