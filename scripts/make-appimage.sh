1d35ada7c7790f60718d0b979d29a5269ebc1fb7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-03-26 12:59:34 +0100

    `ANYLINUX_LIB=1` is automatically set by default now

--- scripts/make-appimage.sh
+++ scripts/make-appimage.sh
@@ -8,7 +8,6 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ANYLINUX_LIB=1
 
 # Deploy dependencies
 quick-sharun \
